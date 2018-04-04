c1aacfe8a231877c7fb82f64af75155b65f2d222
David Dassau <todieahero@hotmail.com>
2018-04-03 19:56:07 -0500

    first working endpoint

--- AmberAndGrain/Controllers/RecipesController.cs
+++ AmberAndGrain/Controllers/RecipesController.cs
@@ -4,10 +4,25 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using AmberAndGrain.Models;
+using AmberAndGrain.Services;
 
 namespace AmberAndGrain.Controllers
 {
+    [RoutePrefix("api/recipes")]
     public class RecipesController : ApiController
     {
+        [Route(""), HttpPost]
+        public HttpResponseMessage AddRecipe(RecipeDto recipe)
+        {
+            var repository = new RecipeRepository();
+            var result = repository.Create(recipe);
+            if (result)
+            {
+                return Request.CreateResponse(HttpStatusCode.Created);
+            }
+
+            return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Could not create recipe, please try again later");
+        }
     }
 }
